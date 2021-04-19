2eff72767d5e26ed3bb0ae998289052927869359
IvanPacanov <mateuszpacan96@interia.eu>
2021-04-19 22:13:31 +0200

    Add project files.

---
+++ src/Services/Users/Users.Domain/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Users.Domain.Common;
+
+namespace Users.Domain.Entities
+{
+    public class User: EntityBase
+    {
+        public string UserName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string Country { get; set; }
+
+
+    }
+}
