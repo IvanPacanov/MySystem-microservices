a31c3f27d0025723f692a021e44af6ec05e8f0a9
Mateusz Pacan <mateuszpacan96@interia.eu>
2021-04-28 09:58:15 +0200

    ForgotPassword

--- src/Andro_Client/app/src/main/java/com/example/appklientvideochat/LoginActivity.kt
+++ src/Andro_Client/app/src/main/java/com/example/appklientvideochat/LoginActivity.kt
@@ -1,11 +1,45 @@
 package com.example.appklientvideochat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        findViewById<TextView>(R.id.textViewSignUp).
+        setOnClickListener(View.OnClickListener {
+            startRegisterActivity();
+        })
+
+        findViewById<TextView>(R.id.forgotPassword).
+        setOnClickListener(View.OnClickListener {
+            startForgotPasswordActivity();
+        })
+
+        findViewById<Button>(R.id.bntlogin).
+        setOnClickListener(View.OnClickListener{
+                    startMainActivity();
+                })
+    }
+
+    private fun startRegisterActivity()
+    {
+        startActivity(Intent(this, RegisterActivity::class.java));
+    }
+
+    private fun startForgotPasswordActivity()
+    {
+        startActivity(Intent(this, PasswordForgotActivity::class.java));
+    }
+
+    private fun startMainActivity()
+    {
+        startActivity(Intent(this, MainActivity::class.java));
     }
 }
---
+++ src/Andro_Client/app/src/main/java/com/example/appklientvideochat/PasswordForgotActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appklientvideochat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PasswordForgotActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_password_forgot)
+    }
+}
