2312d8f3fabd0e6f7950713ca6adbf3a2c691b96
Mateusz Pacan <mateuszpacan96@interia.eu>
2021-05-13 20:37:46 +0200

    RecycleView_Of_UserMessage

--- src/Andro_Client/app/src/main/java/com/example/andro_client/MainActivity.kt
+++ src/Andro_Client/app/src/main/java/com/example/andro_client/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
--- src/Andro_Client/app/src/main/java/com/example/andro_client/ui/home/HomeFragment.kt
+++ src/Andro_Client/app/src/main/java/com/example/andro_client/ui/home/HomeFragment.kt
@@ -8,7 +8,11 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.andro_client.R
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
 
 class HomeFragment : Fragment() {
 
@@ -22,10 +26,36 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val recycleView: RecyclerView = root.findViewById(R.id.recycleview_message)
+
+        val adapter = GroupAdapter<ViewHolder>()
+
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+
+
+        recycleView.adapter = adapter
+//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
         return root
     }
 }
+
+class UserItem: Item<ViewHolder>() {
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+   //     TODO("Not yet implemented")
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.user_row_new_message
+    }
+}
+
+//class CustomAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>{
+//    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+//        TODO("Not yet implemented")
+//    }
+//
+//}
