95c528f30dcfd0ef0036e48479516c29decc359f
Mateusz Pacan <mateuszpacan96@interia.eu>
2021-05-18 00:33:51 +0200

    Start working on chats

---
+++ src/Services/Chat/Chat.Domain/Entities/Chats.cs
@@ -0,0 +1,13 @@
+﻿using Chat.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Domain.Entities
+{
+   public class Chats : EntityBase
+    {
+    }
+}
---
+++ src/Services/Chat/Chat.Infrastructure/Persistence/ChatContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chat.Domain.Entities;
+
+
+
+namespace Chat.Infrastructure.Persistence
+{
+    public class ChatContext : DbContext
+    {
+
+        public ChatContext(DbContextOptions<ChatContext> option): base(option)
+        {
+
+        }
+
+        public DbSet<Chats> Chat { get; set; }
+    }
+}
